42b90d7c6cba794e77fca532b923fe4ea235ef06
bwfadmin <bwfadmin@bwfadmin-PC>
2017-06-20 18:28:38 +0800

    ccc

--- codeDemo/src/com/bwf/Test.java
+++ codeDemo/src/com/bwf/Test.java
@@ -6,5 +6,6 @@ public class Test {
 		System.out.println("---222aaa-------");
 		System.out.println("3333333333333");
 		System.out.println("4444444444444");
+		System.out.println("4444444444444aaa");
 	}
 }
